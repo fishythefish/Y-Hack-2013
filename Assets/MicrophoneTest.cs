d2cf1fde71cc637fe95dcc9f36ec7631e05a6fab
fishythefish <mayank.patke@gmail.com>
2013-11-08 23:43:48 -0500

    .

---
+++ Assets/MicrophoneTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicrophoneTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		audio.clip = Microphone.Start(Microphone.devices[0], true, 1, 44100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
