5e0a6f71f08f1e41779d08184000350e699c6d03
fishythefish <mayank.patke@gmail.com>
2013-11-08 22:23:19 -0500

    .

---
+++ Assets/MicrophoneFFT.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicrophoneFFT : MonoBehaviour {
+	
+	public AudioClip audioClip;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
