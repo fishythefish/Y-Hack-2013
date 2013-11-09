762746d455a5521eaa58db953834c07ccd4cc42a
Andrew Rochman <aokiroch@yahoo.com>
2013-11-09 13:07:18 -0500

    commit

---
+++ Assets/eat.js
@@ -0,0 +1,13 @@
+﻿function OnCollisionEnter(hit : Collision) {
+	
+	if (hit.gameObject.name == "dot") {
+		Destroy(hit.gameObject);
+	}
+	
+}
+
+function OnTriggerEnter (other : Collider) {
+		if (other.gameObject.name == "Sphere(Clone)"){
+			Destroy(other.gameObject);
+		}
+}
---
+++ Assets/generatedots.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+	var dot = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+	dot.transform.collider.isTrigger = true;
+	
+	for (var i = -14; i < 12; ++i) {
+		for (var j = 0; j < 29; ++j)
+		Instantiate(dot, Vector3(39 + (3.8 * i), 2, 103.5 - (3.9 * j)), Quaternion.identity);
+	}
+}
+
+function Update () {
+
+}
